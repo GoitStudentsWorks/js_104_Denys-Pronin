2e4a156ec092e97226a2b7f9be0d43b45359faac
LiudmylaKobryn <133100987+LiudmylaKobryn@users.noreply.github.com>
2024-06-16 19:16:05 +0200

    upd

---
+++ src/js/faq.js
@@ -0,0 +1,22 @@
+import Accordion from 'accordion-js';
+
+const according = document.querySelector('.faq-list');
+const listItemAc = document.querySelectorAll('.ac');
+
+new Accordion(according, {
+  openOnInit: [0],
+  collapse: true,
+  duration: 400,
+  showMultiple: false,
+  beforeOpen: function (currentElement) {
+    if (currentElement.previousElementSibling) {
+      currentElement.previousElementSibling.classList.add('no-border-bottom');
+    }
+    currentElement.classList.add('no-border-bottom');
+  },
+  beforeClose: function () {
+    listItemAc.forEach(elem => {
+      elem.classList.remove('no-border-bottom');
+    });
+  },
+});
