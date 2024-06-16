30be5b2de801dc0537a9cdccb0731944c4e0c7ec
LiudmylaKobryn <133100987+LiudmylaKobryn@users.noreply.github.com>
2024-06-16 20:54:00 +0200

    upd

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -3,12 +3,9 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 document
   .querySelector('.work-together-form')
-  .addEventListener('submit', async event => {
+  .addEventListener('input', async event => {
     event.preventDefault();
-
     const emailInput = document.querySelector('.work-together-email');
-    const commentInput = document.querySelector('.work-together-comment');
-
     const isValidEmail = emailInput.checkValidity();
 
     if (!isValidEmail) {
@@ -17,47 +14,50 @@ document
     } else {
       emailInput.classList.remove('invalid');
     }
-
-    const requestData = {
-      email: emailInput.value,
-      comment: commentInput.value,
-    };
-
-    try {
-      const response = await axios.post(
-        'https://portfolio-js.b.goit.study/api/requests',
-        requestData,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-          },
-        }
-      );
-
-      if (response.status === 201) {
-        showModal();
-        document.querySelector('.work-together-form').reset();
-      } else {
-        iziToast.error({
-          title: 'Error',
-          message: `Помилка: ${response.data.message}`,
-        });
-      }
-    } catch (error) {
-      if (error.response) {
-        iziToast.error({
-          title: 'Error',
-          message: `Помилка: ${error.response.data.message}`,
-        });
-      } else {
-        iziToast.error({
-          title: 'Error',
-          message: 'Виникла помилка при відправці запиту. Спробуйте ще раз.',
-        });
-      }
-    }
   });
+document.addEventListener('submit', async event => {
+  event.preventDefault();
+  const commentInput = document.querySelector('.work-together-comment');
+  const requestData = {
+    email: emailInput.value,
+    comment: commentInput.value,
+  };
+
+  try {
+    const response = await axios.post(
+      'https://portfolio-js.b.goit.study/api/requests',
+      requestData,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      }
+    );
+
+    if (response.status === 201) {
+      showModal();
+      document.querySelector('.work-together-form').reset();
+    } else {
+      iziToast.error({
+        title: 'Error',
+        message: `Помилка: ${response.data.message}`,
+      });
+    }
+  } catch (error) {
+    if (error.response) {
+      iziToast.error({
+        title: 'Error',
+        message: `Помилка: ${error.response.data.message}`,
+      });
+    } else {
+      iziToast.error({
+        title: 'Error',
+        message: 'Виникла помилка при відправці запиту. Спробуйте ще раз.',
+      });
+    }
+  }
+});
 
 document.querySelector('.modal-close').addEventListener('click', () => {
   closeModal();
