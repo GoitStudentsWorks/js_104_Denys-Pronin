f41b7d3ff7faf76d8322dc8a1fa3e010a282f3a0
Denys Pronin <denispronindev@gmail.com>
2024-06-18 00:47:09 +0300

    UPD img

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -1,25 +1,56 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const projects = [
-        { image: './img/my-projects/Rectangle-10.jpg', tech: 'React, JavaScript, Node JS, Git', name: 'power pulse webservice ' },
-        { image: './img/my-projects/Rectangle-7.jpg', tech: 'Angular, TypeScript, Node JS, Git', name: 'mimino website' },
-        { image: './img/my-projects/Rectangle-8.jpg', tech: 'Vue, JavaScript, Node JS, Git', name: 'vyshyvanka vibes Landing Page' },
-        { image: './img/my-projects/Rectangle-6.jpg', tech: 'React Native, JavaScript, Git', name: 'chego jewelry website' },
-        { image: './img/my-projects/Rectangle-4.jpg', tech: 'Svelte, JavaScript, Node JS, Git', name: 'energy flow webservice' },
-        { image: './img/my-projects/Rectangle-5.jpg', tech: 'React, JavaScript, Express, MongoDB', name: 'fruitbox online store' },
-        { image: './img/my-projects/first-screen-1.jpg', tech: 'React, JavaScript, Express, MongoDB', name: 'starlight studio landing page' }
-    ];
-    
-    const projectsContainer = document.getElementById('project-container');
-    const loadMoreButton = document.getElementById('load-more-button');
-    let projectsLoaded = 0;
-    const projectsToLoad = 3;
+document.addEventListener('DOMContentLoaded', function () {
+  const projects = [
+    {
+      image: './img/my-projects/Rectangle-10.jpg',
+      tech: 'React, JavaScript, Node JS, Git',
+      name: 'power pulse webservice ',
+    },
+    {
+      image: './img/my-projects/Rectangle-7.jpg',
+      tech: 'Angular, TypeScript, Node JS, Git',
+      name: 'mimino website',
+    },
+    {
+      image: './img/my-projects/Rectangle-8.jpg',
+      tech: 'Vue, JavaScript, Node JS, Git',
+      name: 'vyshyvanka vibes Landing Page',
+    },
+    {
+      image: './img/my-projects/Rectangle-6.jpg',
+      tech: 'React Native, JavaScript, Git',
+      name: 'chego jewelry website',
+    },
+    {
+      image: './img/my-projects/Rectangle-4.jpg',
+      tech: 'Svelte, JavaScript, Node JS, Git',
+      name: 'energy flow webservice',
+    },
+    {
+      image: './img/my-projects/Rectangle-5.jpg',
+      tech: 'React, JavaScript, Express, MongoDB',
+      name: 'fruitbox online store',
+    },
+    {
+      image: './img/my-projects/first-screen-1.jpg',
+      tech: 'React, JavaScript, Express, MongoDB',
+      name: 'starlight studio landing page',
+    },
+  ];
 
-    function loadProjects() {
-        const nextProjects = projects.slice(projectsLoaded, projectsLoaded + projectsToLoad);
-        nextProjects.forEach(project => {
-            const projectItem = document.createElement('li');
-            
-            projectItem.innerHTML = `
+  const projectsContainer = document.getElementById('project-container');
+  const loadMoreButton = document.getElementById('load-more-button');
+  let projectsLoaded = 0;
+  const projectsToLoad = 3;
+
+  function loadProjects() {
+    const nextProjects = projects.slice(
+      projectsLoaded,
+      projectsLoaded + projectsToLoad
+    );
+    nextProjects.forEach(project => {
+      const projectItem = document.createElement('li');
+
+      projectItem.innerHTML = `
 
             <div class="image-wrapper">
                 <img src="${project.image}" alt="${project.name}">
@@ -30,24 +61,24 @@ document.addEventListener('DOMContentLoaded', function() {
                 <h3 class="name-project">${project.name}</h3>
                 <div class="link-container">
                     <a  target="_blank" class="link-project" href="https://github.com/Denys-Pronin/IT-Hub-Portfolio">VISIT
-                        <svg stroke="#00B068" width="24" height="24"><use href="../img/symbol-defs.svg#icon-vector"></use></svg>
+                        <svg stroke="#00B068" width="24" height="24"><use href="./img/symbol-defs.svg#icon-vector"></use></svg>
                     </a>
                 </div>
                 </div>
             </div>
-       
+
 
 `;
-            projectsContainer.appendChild(projectItem);
-        });
-        projectsLoaded += projectsToLoad;
-        if (projectsLoaded >= projects.length) {
-            loadMoreButton.style.display = 'none';
-        }
+      projectsContainer.appendChild(projectItem);
+    });
+    projectsLoaded += projectsToLoad;
+    if (projectsLoaded >= projects.length) {
+      loadMoreButton.style.display = 'none';
     }
+  }
 
-    loadMoreButton.addEventListener('click', loadProjects);
+  loadMoreButton.addEventListener('click', loadProjects);
 
-    // Завантажити початкові проекти
-    loadProjects();
+  // Завантажити початкові проекти
+  loadProjects();
 });
